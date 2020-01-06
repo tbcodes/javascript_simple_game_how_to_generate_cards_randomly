30e78e2db3378cf8f8d65297820535c5e4f6a232
TruzzBlogg <soypacos@hotmail.com>
2020-01-07 00:40:48 +0100

    HTML, CSS and JS files uploaded!

---
+++ script.js
@@ -0,0 +1,59 @@
+let cards = ['♥', '♣', '♦', '♠'];
+let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']; 
+
+
+function displayCard() {
+  let singleCard = cards[Math.floor(Math.random() * cards.length)];
+  let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let showCard = `${singleNumber}   ${singleCard}`;
+  document.getElementById("showing").style.background = '#0accff';
+  document.getElementById("showing").innerHTML = showCard;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayCard() {
+//   let singleCard = cards[Math.floor(Math.random() * cards.length)];
+//   let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+//   let show = `${singleNumber}   ${singleCard}`;
+//   document.getElementById("showing").style.background = '#0accff';
+//   document.getElementById("showing").innerHTML = show;
+// }
